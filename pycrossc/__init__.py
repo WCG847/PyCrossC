879f00b9ff3649225e2fe2c35a96e1224d3d9128
WCG847 <heelmods@gmail.com>
2025-06-29 11:17:31 +0100

    Add Python wrappers for C printf/sprintf and update imports
    Introduced Python wrappers for C `printf` and `sprintf` using the
    `cffi` library, enabling formatted output and string formatting.
    Added `printf.py` and `sprintf.py` modules, updated project
    configuration in `PyCrossC.pyproj` to include these files, and
    changed the startup file to `pycrossc\printf.py`.
    
    Refactored `__init__.py` to replace wildcard imports with explicit
    imports for improved clarity and maintainability. Updated imports
    to include new `printf` and `sprintf` modules. These changes
    enhance functionality and code organization.

--- pycrossc/__init__.py
+++ pycrossc/__init__.py
@@ -1,3 +1,5 @@
-from .Global import *
+from .Global import __register_global_object, __global_destructor_chain
 from .malloc import *
-from .sinit import *
+from .sinit import __call_static_initializers, __initialize_cpp_rts
+from .printf import printf
+from .sprintf import sprintf
